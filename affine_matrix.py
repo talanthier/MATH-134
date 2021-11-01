fe65b51171d0e104154de7ea2af268d366ca202e
talanthier <talanthier@gmail.com>
2021-10-31 20:11:54 -0700

    added inverse mapping from message units in Z/30Z to our alphabet

--- affine_matrix.py
+++ affine_matrix.py
@@ -1,13 +1,16 @@
 import numpy as np
 import string
 
-enciphered_message = list(open('sec_5_enciphered_message.txt', 'r').read())
+enciphered_message = list(open('sec_5_enciphered_message.txt', 'r').read()) # reads in our enciphered message from text file
 
 alphabet = list(string.ascii_uppercase)
 alphabet.extend([' ',',','.','?']) # adds ` ` `,` `.`and `?` to our alphabet
 
-translation_dict = dict(zip(alphabet, np.arange(30))) # dictionary from letters to elements in Z/30Z
-enc_num = list(map(translation_dict.get, enciphered_message))
+letters_to_num_dict = dict(zip(alphabet, np.arange(30))) # dictionary from letters to elements in Z/30Z
+num_to_letters_dict = {v: k for k, v in letters_to_num_dict.items()}
+
+
+enc_num = list(map(letters_to_num_dict.get, enciphered_message))
 
 A = np.array([[14,5],[3,11]]) # computed in earlier problem
 A_inv = (-11*np.array([[11,-5],[-3,14]]))%30 # computed by hand in Z/30Z
@@ -16,12 +19,9 @@ enc_num = np.reshape(enc_num, [2, int(len(enc_num)/2)], 'F')
 # puts enciphered message units into 2 x length(enc_num) matrix
 
 message_num = np.dot(A_inv,enc_num)
-message_num = message_num.flatten('F') % 30
+message_num = message_num.flatten('F') % 30 # flattens our matrix to 1-d array
 
-message = ''
+msg = ''
+msg = msg.join(map(num_to_letters_dict.get, message_num)) # maps message_num back to our alphabet
 
-for i in range(len(message_num)):
-	for j in range(len(alphabet)):
-		if message_num[i] == nums[j]:
-			message += alphabet[j]
-print(message)
+print(msg)
